53d9630ca4360fe31faf31663846a86b223923ad
Daniel Sander Isaksen <daniesis@student.matnat.uio.no>
2020-03-12 10:37:53 +0100

    ctrnn 100 gens game width 32

---
+++ source/cont_exp/evoalg.py
@@ -0,0 +1,143 @@
+import sys
+import os
+import numpy as np
+import re
+
+import neat
+import visualize
+from neat import activations
+from game import Game
+
+time_const = 0.01
+
+def eval_genomes(genomes, config):
+    num_games = 10
+    for genime_id, genome in genomes:
+        genome.fitness = 0
+        net = neat.ctrnn.CTRNN.create(genome, config, time_const)
+        game = Game(32)
+        for i in range(num_games):
+            genome.fitness += game.run(net)
+
+# New activation functions
+def OR_gate(x):
+    try:
+        if any(x) > 0.5:
+            return 1
+        else:
+            return 0
+    except TypeError as te:
+        if x > 0.5:
+            return 1
+        else:
+            return 0
+
+def AND_gate(x):
+    try:
+        if all(x) > 0.5:
+            return 1
+        else:
+            return 0
+    except TypeError as te:
+        if x > 0.5:
+            return 1
+        else:
+            return 0
+
+def XOR_gate(x):
+    try:
+        if len(x[x > 0]) == 1:
+            return 1
+        else:
+            return 0
+    except TypeError as te:
+        if x > 0.5:
+            return 1
+        else:
+            return 0
+
+
+def run(config_file):
+    # Load configuration
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+            neat.DefaultSpeciesSet, neat.DefaultStagnation,
+            config_file)
+
+
+    config.genome_config.add_activation('OR_gate', OR_gate)
+    config.genome_config.add_activation('AND_gate', AND_gate)
+    config.genome_config.add_activation('XOR_gate', XOR_gate)
+
+    print(f'init pop')
+    # Create the population, which is the top-level object for a NEAT run.
+    p = neat.Population(config)
+
+    # Add a stdout reporter to show progress in the terminal
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    p.add_reporter(neat.Checkpointer(5))
+
+    # Run for up to 300 generations.
+    num_gen = 100
+    winner = p.run(eval_genomes, num_gen)
+
+    # Display the winning genome
+    print(f'\nBest genome:\n{winner}')
+
+    # Show output of the most fit genome against training data.
+    print('\nOutput:')
+    game = Game(32)
+    winner_net = neat.ctrnn.CTRNN.create(winner, config, time_const)
+    print(f'WINNER FITNESS {winner.fitness}')
+    winner.fitness = 0
+    i = 0
+    num_games = 10
+    while i < num_games:
+        ret = game.run_print(winner_net)
+        print(f'Game returned {ret}')
+        winner.fitness += ret
+        i+=1
+    print(f'\nOver {num_games} games, a winner scored {winner.fitness}.\n')
+
+    print(f'node evals {winner_net.node_evals}')
+    print(f'input nodes {winner_net.input_nodes}')
+    print(f'output nodes {winner_net.output_nodes}')
+    #Using regex to find the names ofv
+    p = re.compile('(AND|OR|XOR)')
+    node_names = {-1:'IA', -2:'IB', 0:'OA', 1:'OB'}
+    '''
+    for node, activation, aggregation, bias, response, links in winner_net.node_evals:
+        print(f'NODE NUMBER IS {node}')
+        if node not in node_names.keys():
+            node_names[node] = p.search(str(activation)).group(0)
+        else:
+            node_names[node] = str(node_names[node]) + '\n' + str(p.search(str(activation)).group(0))
+    '''
+
+
+    visualize.draw_net(config, winner, True, node_names=node_names)
+    visualize.plot_stats(stats, ylog=False, view=True)
+    visualize.plot_species(stats, view=True)
+
+    p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-4')
+    p.run(eval_genomes, 10)
+if __name__ == '__main__':
+    # Detemine path to configuration file. This path manipulation is
+
+    # here so that the script will run sucessfully regardless of the
+    # current working directory.
+
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config-ctrnn')
+
+    # redirect print
+    original = sys.stdout
+    log_path = os.path.join(local_dir, 'ctrnn.log')
+    sys.stdout = open('ctrnn.log', 'w')
+
+    # run program
+    run(config_path)
+
+    # redirect print
+    sys.stdout = original
---
+++ source/cont_exp/game.py
@@ -0,0 +1,162 @@
+import numpy as np
+import random
+
+class Game:
+    '''
+    For now the block only has the size of one unit
+    '''
+    def __init__(self, game_width):
+        self.game_width = game_width
+        self.game_height = 3*game_width
+
+        # The height is three times the width, to make the game possible.
+        self.board = np.zeros((self.game_height, self.game_width))
+
+    #def update_state(self):
+    def _update_paddle(self, motor_out):
+        if motor_out[0] > 0.8:
+            self.paddle_pos = self.paddle_pos
+        elif motor_out[0] > 0.7 and self.paddle_pos < self.game_width-1:
+            self.paddle_pos += 1
+        elif motor_out[0] > 0.4 and self.paddle_pos > 0:
+            self.paddle_pos -= 1
+        elif motor_out[0] > 0.1:
+            self.paddle_pos = self.paddle_pos
+        elif motor_out[0] > 1E-20 and self.paddle_pos > 0 and self.paddle_pos < self.game_width-1:
+            self.paddle_pos = random.randint(-1,1)
+
+
+    def _update_block(self):
+            self.board[self.block_pos[0]][self.block_pos[1]] = 0 # remove block from old pos
+
+            # Update position
+            self.block_pos[0] += 1 # move down
+            if self.block_pos[1] > 0 and self.block_pos[1] < self.game_width-1:
+                self.block_pos[1] = random.randint(self.block_pos[1] - 1, self.block_pos[1] + 1) # move left or right
+            elif self.block_pos[1] == 0:
+                self.block_pos[1] += 1 # move right
+            elif self.block_pos[1] == self.game_width-1:
+                self.block_pos[1] -= 1 # move left
+
+            self.board[self.block_pos[0]][self.block_pos[1]] = 1 # set block in new pos
+
+    def _print_game(self):
+        vizboard = self.board
+        vizboard[self.game_height-1, :] = 0
+        vizboard[self.game_height-1][self.paddle_pos] = 1
+        print(vizboard)
+
+
+    def run(self, animat):
+        '''
+        It takes an animat player to play the game.
+        '''
+
+        init1 = 0.0
+        init2 = 0.0
+
+        animat.set_node_value(-1, init1)
+        animat.set_node_value(-2, init2)
+
+        times = [0.0]
+        outputs = [[init1, init2]]
+
+        #RESET GAME
+        self.block_pos = [0, random.randint(0,self.game_width-1)] # postion in y,x / rows, cols
+        self.board[self.block_pos[0]][self.block_pos[1]] = 1
+        self.paddle_pos = int(self.game_width/2) # along the x-axis / cols
+
+        while self.block_pos[0] < self.game_height-1: # until the block is at the bottom of the board
+            self._update_block()
+
+            for i in range(10):
+                # The animat views the board with its two sensors. Has a view of one unit to the left or right.
+                if self.block_pos[1] == self.paddle_pos:
+                    sens_in = [1,1]
+                    #print("sensed block on both sensors")
+                elif self.block_pos[1] - 1 == self.paddle_pos:
+                    #print("sensed block on right sensor")
+                    sens_in = [1,0]
+                elif self.block_pos[1] + 1 == self.paddle_pos:
+                    sens_in = [0,1]
+                    #print("sensed block on left sensor")
+                else:
+                    sens_in = [0,0]
+                    #print("didnt sense block")
+
+                output = animat.advance(sens_in, 0.002, 0.002)
+                times.append(animat.time_seconds)
+                outputs.append(output)
+                self._update_paddle(outputs[-1])
+            #self._print_game()
+        #print(f'MOTOR OUT: {outputs}')
+
+        if self.block_pos[0] == self.game_height-1:
+            if self.paddle_pos-1 == self.block_pos[1] or self.paddle_pos == self.block_pos[1] or self.paddle_pos+1 == self.block_pos[1]: # paddle size is 3
+                return 1 # point
+            else:
+                return 0
+
+    def run_print(self, animat):
+        '''
+        It takes an animat player to play the game.
+        '''
+
+        init1 = 0.0
+        init2 = 0.0
+
+        animat.set_node_value(-1, init1)
+        animat.set_node_value(-2, init2)
+
+        times = [0.0]
+        outputs = [[init1, init2]]
+
+        #RESET GAME
+        self.block_pos = [0, random.randint(0,self.game_width-1)] # postion in y,x / rows, cols
+        self.board[self.block_pos[0]][self.block_pos[1]] = 1
+        self.paddle_pos = int(self.game_width/2) # along the x-axis / cols
+
+        while self.block_pos[0] < self.game_height-1: # until the block is at the bottom of the board
+            self._update_block()
+
+            for i in range(10):
+                # The animat views the board with its two sensors. Has a view of one unit to the left or right.
+                if self.block_pos[1] == self.paddle_pos:
+                    sens_in = [1,1]
+                    print("sensed block on both sensors")
+                elif self.block_pos[1] - 1 == self.paddle_pos:
+                    print("sensed block on right sensor")
+                    sens_in = [1,0]
+                elif self.block_pos[1] + 1 == self.paddle_pos:
+                    sens_in = [0,1]
+                    print("sensed block on left sensor")
+                else:
+                    sens_in = [0,0]
+                    print("didnt sense block")
+
+                #self._print_game()
+                print(f'PADDLE POS IS {self.paddle_pos}')
+                print(f'BLOCK POS is {self.block_pos[1]}')
+
+                output = animat.advance(sens_in, 0.002, 0.002)
+                times.append(animat.time_seconds)
+                outputs.append(output)
+                self._update_paddle(outputs[-1])
+                print(f'MOTOR OUT: {outputs[-1]}')
+            #self._print_game()
+        #print(f'MOTOR OUT: {outputs}')
+
+        if self.block_pos[0] == self.game_height-1:
+            if self.paddle_pos-1 == self.block_pos[1] or self.paddle_pos == self.block_pos[1] or self.paddle_pos+1 == self.block_pos[1]: # paddle size is 3
+                return 1 # point
+            else:
+                return 0
+
+if __name__ == '__main__':
+    num_nodes = 8
+    animat = Animat(num_nodes)
+    game_width = 8
+    game = Game(game_width)
+    game.run(animat)
+
+
