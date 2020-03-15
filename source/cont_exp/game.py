20814bf2e9c5de164c2f765d0b3bcc3b268a8701
Daniel Sander Isaksen <daniesis@student.matnat.uio.no>
2020-03-15 12:30:33 +0100

    200 generations with bigger/lesser than comparison for action mechanic

--- source/cont_exp/game.py
+++ source/cont_exp/game.py
@@ -14,11 +14,15 @@ class Game:
 
     #def update_state(self):
     def _update_paddle(self, motor_out):
-        if motor_out[0] > 0.4 and motor_out[1] > 0.4:
+        if motor_out[0] > 0.6 and motor_out[1] > 0.6:
             self.paddle_pos = self.paddle_pos
         elif motor_out[1] > motor_out[0] and self.paddle_pos < self.game_width-1:
             self.paddle_pos += 1
         elif motor_out[0] > motor_out[1] and self.paddle_pos > 0:
+            self.paddle_pos -= 1
+        elif motor_out[0] < 0.1 and self.paddle_pos == 0:
+            self.paddle_pos += 1
+        elif motor_out[1] < 0.1 and self.paddle_pos == self.game_width-1:
             self.paddle_pos -= 1
         '''
         elif motor_out[0] < 0.1 and motor_out[1] < 0.1:
