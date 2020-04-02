539bc37337fbe60a5ef2d91cc541c0c29d4ec155
Daniel Sander Isaksen <daniesis@student.matnat.uio.no>
2020-04-02 16:27:25 +0200

    500gen 400popsize 2in4hi2ou nowrap catch+avoid no penalties, reset every other, no condition sends input on both sensors at once, same activation and aggregation as the previus one, FEED FORWARD with recursive...

--- source/discrete_experiment/evoalg.py
+++ source/discrete_experiment/evoalg.py
@@ -6,35 +6,38 @@ import re
 import neat
 import visualize
 from neat import activations
-from animat import Animat
 from game import Game
 
+time_const = 0.01
+
 def eval_genomes(genomes, config):
-    num_games = 100
+    num_games = 50
     for genime_id, genome in genomes:
         genome.fitness = 0
-        net = neat.nn.RecurrentNetwork.create(genome, config)
-        game = Game(64)
+        net = neat.nn.feed_forward.FeedForwardNetwork.create(genome, config)
+        game = Game(8)
         for i in range(num_games):
             genome.fitness += game.run(net)
 
+# Aggregation functions o
+#TODO: Check what x can be...
 # New activation functions
 def OR_gate(x):
     try:
-        if any(x) > 0.5:
-            return 1
+        if any(x) == 1:
+            return np.max(x)
         else:
             return 0
     except TypeError as te:
         if x > 0.5:
-            return 1
+            return x
         else:
             return 0
 
 def AND_gate(x):
     try:
         if all(x) > 0.5:
-            return 1
+            return np.mean(x)
         else:
             return 0
     except TypeError as te:
@@ -43,18 +46,36 @@ def AND_gate(x):
         else:
             return 0
 
+#TODO: Tror ikke at denne fungerer... Checkout out aggregation..
 def XOR_gate(x):
     try:
         if len(x[x > 0]) == 1:
-            return 1
+            return np.max(x)
         else:
             return 0
     except TypeError as te:
         if x > 0.5:
-            return 1
+            return x
         else:
             return 0
 
+def or_aggregation(x):
+    if any(x) == 1:
+        return 1
+    else:
+        return 0
+
+def and_aggregation(x):
+    if all(x) == 1:
+        return 1
+    else:
+        return 0
+
+def xor_aggregation(x):
+    if x.count(1) == 1:
+        return 1
+    else:
+        return 0
 
 def run(config_file):
     # Load configuration
@@ -66,10 +87,16 @@ def run(config_file):
     config.genome_config.add_activation('OR_gate', OR_gate)
     config.genome_config.add_activation('AND_gate', AND_gate)
     config.genome_config.add_activation('XOR_gate', XOR_gate)
+    config.genome_config.add_aggregation('or_aggregation', or_aggregation)
+    config.genome_config.add_aggregation('and_aggregation', and_aggregation)
+    config.genome_config.add_aggregation('xor_aggregation', xor_aggregation)
 
-    print(f'init pop')
+    #print(f'init pop')
     # Create the population, which is the top-level object for a NEAT run.
     p = neat.Population(config)
+    # Restore from checkpoint
+    #print(f"restore pop")
+    #p = neat.Checkpointer.restore_checkpoint("neat-checkpoint-59")
 
     # Add a stdout reporter to show progress in the terminal
     p.add_reporter(neat.StdOutReporter(True))
@@ -78,60 +105,63 @@ def run(config_file):
     p.add_reporter(neat.Checkpointer(5))
 
     # Run for up to 300 generations.
-    num_gen = 100
+    num_gen = 500
     winner = p.run(eval_genomes, num_gen)
+
 
     # Display the winning genome
     print(f'\nBest genome:\n{winner}')
 
     # Show output of the most fit genome against training data.
     print('\nOutput:')
-    game = Game(64)
-    winner_net = neat.nn.RecurrentNetwork.create(winner, config)
+    game = Game(8)
+    winner_net = neat.nn.feed_forward.FeedForwardNetwork.create(winner, config)
     print(f'WINNER FITNESS {winner.fitness}')
     winner.fitness = 0
     i = 0
-    num_games = 100
+    num_games = 50
     while i < num_games:
-        ret = game.run_print(winner_net)
+        ret = game.run(winner_net)
         print(f'Game returned {ret}')
         winner.fitness += ret
         i+=1
     print(f'\nOver {num_games} games, a winner scored {winner.fitness}.\n')
 
+
+    print(f'input nodes {winner_net.input_nodes}')
+    print(f'output nodes {winner_net.output_nodes}')
     #Using regex to find the names ofv
     p = re.compile('(AND|OR|XOR)')
     node_names = {-1:'IA', -2:'IB', 0:'OA', 1:'OB'}
+    '''
     for node, activation, aggregation, bias, response, links in winner_net.node_evals:
         print(f'NODE NUMBER IS {node}')
         if node not in node_names.keys():
             node_names[node] = p.search(str(activation)).group(0)
         else:
             node_names[node] = str(node_names[node]) + '\n' + str(p.search(str(activation)).group(0))
+    '''
 
-    print(f'node evals {winner_net.node_evals}')
-    print(f'input nodes {winner_net.input_nodes}')
-    print(f'output nodes {winner_net.output_nodes}')
 
-    visualize.draw_net(config, winner, True, node_names=node_names)
+    visualize.draw_net(config, winner, True, node_names=node_names, show_disabled=False, prune_unused=True)
     visualize.plot_stats(stats, ylog=False, view=True)
     visualize.plot_species(stats, view=True)
 
     p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-4')
     p.run(eval_genomes, 10)
-
 if __name__ == '__main__':
     # Detemine path to configuration file. This path manipulation is
+
     # here so that the script will run sucessfully regardless of the
     # current working directory.
 
     local_dir = os.path.dirname(__file__)
-    config_path = os.path.join(local_dir, 'config-discrete')
+    config_path = os.path.join(local_dir, 'config-ff')
 
     # redirect print
     original = sys.stdout
-    log_path = os.path.join(local_dir, 'log-discrete')
-    sys.stdout = open('log_path', 'w')
+    log_path = os.path.join(local_dir, 'ffnn.log')
+    sys.stdout = open('ffnn.log', 'w')
 
     # run program
     run(config_path)
--- source/discrete_experiment/game.py
+++ source/discrete_experiment/game.py
@@ -1,13 +1,13 @@
 import numpy as np
 import random
 
-from animat import Animat
-
 class Game:
     '''
     For now the block only has the size of one unit
     '''
     def __init__(self, game_width):
+        self.game_cnt = 0
+        self.direction = 0
         self.game_width = game_width
         self.game_height = 3*game_width
 
@@ -16,47 +16,56 @@ class Game:
 
     #def update_state(self):
     def _update_paddle(self, motor_out):
-        # moves positon along axis of freedom, checks for boundaries
-        if motor_out[0] > motor_out[1] and self.paddle_pos > 0:
-            self.paddle_pos -= 1
-        elif motor_out[0] < motor_out[1] and self.paddle_pos < self.game_width-1:
-            self.paddle_pos += 1
-        elif motor_out[0] < 1E-3 and motor_out[1] < 1E-3:
-            if self.paddle_pos == 0:
-                self.paddle_pos += 1
-            elif self.paddle_pos == self.game_width-1:
-                self.paddle_pos -= 1
+        if motor_out[0] == 0 and motor_out[1] == 0:
+            self.paddle_pos = self.paddle_pos
+        elif motor_out[0] == 1 and motor_out[1] == 1:
+            self.paddle_pos = self.paddle_pos
+        elif motor_out[1] == 1:
+            if self.paddle_pos == self.game_width-1:
+                self.paddle_pos = self.paddle_pos
             else:
-                self.paddle_pos += random.randint(-1,1)
-        '''
-        if motor_out[0] > 0.5 and motor_out[1] > 0.5:
-            self.paddle_pos = self.paddle_pos
-        elif motor_out[1] > 0.5 and self.paddle_pos < self.game_width-1:
-            self.paddle_pos += 1 # move right
-        elif motor_out[0] > 0.5 and self.paddle_pos > 0:
-            self.paddle_pos -= 1 # move left
-        else:
-            self.paddle_pos = self.paddle_pos
-        '''
+                self.paddle_pos += 1
+        elif motor_out[0] == 1:
+            if self.paddle_pos == 0:
+                self.paddle_pos = self.paddle_pos
+            else:
+                self.paddle_pos -= 1
 
     def _update_block(self):
-            self.board[self.block_pos[0]][self.block_pos[1]] = 0 # remove block from old pos
+        if self.game_cnt%3 == 0:
+            self.direction = random.randint(-1,1)
 
-            # Update position
-            self.block_pos[0] += 1 # move down
-            if self.block_pos[1] > 0 and self.block_pos[1] < self.game_width-1:
-                self.block_pos[1] = random.randint(self.block_pos[1] - 1, self.block_pos[1] + 1) # move left or right
-            elif self.block_pos[1] == 0:
-                self.block_pos[1] += 1 # move right
-            elif self.block_pos[1] == self.game_width-1:
-                self.block_pos[1] -= 1 # move left
+        #self.board[self.block_pos[0]][self.block_pos[1]] = 0 # remove block from old pos
 
-            self.board[self.block_pos[0]][self.block_pos[1]] = 1 # set block in new pos
+        # Update position
+        self.block_pos[0] += 1 # move down
+        if self.block_pos[1] == self.game_width - 1:
+            if self.direction == 1:
+                self.block_pos[1] = self.block_pos[1]
+            elif self.direction == -1:
+                self.block_pos[1] = self.game_width - 2
+            elif self.direction == 0:
+                self.block_pos[1] = self.block_pos[1]
+        elif self.block_pos[1] == 0:
+            if self.direction == 1:
+                self.block_pos[1] = 1
+            elif self.direction == -1:
+                self.block_pos[1] = self.block_pos[1]
+            elif self.direction == 0:
+                self.block_pos[1] = self.block_pos[1]
+        else:
+            self.block_pos[1] += self.direction
+
+        #self.board[self.block_pos[0]][self.block_pos[1]] = 1 # set block in new pos
+
+        self.game_cnt += 1
+        #print(f'(updated game count: {self.game_cnt}')
 
     def _print_game(self):
         vizboard = self.board
-        vizboard[self.game_height-1, :] = 0
-        vizboard[self.game_height-1][self.paddle_pos] = 1
+        vizboard[:][:] = 0
+        vizboard[self.block_pos[0]][self.block_pos[1]] = 1
+        vizboard[self.game_height-1][self.paddle_pos] = 2
         print(vizboard)
 
 
@@ -64,72 +73,102 @@ class Game:
         '''
         It takes an animat player to play the game.
         '''
+
         #RESET GAME
         self.block_pos = [0, random.randint(0,self.game_width-1)] # postion in y,x / rows, cols
-        self.board[self.block_pos[0]][self.block_pos[1]] = 1
+        self.block_size = random.randint(1,2)
+        #self.board[self.block_pos[0]][self.block_pos[1]] = 1
         self.paddle_pos = int(self.game_width/2) # along the x-axis / cols
 
         while self.block_pos[0] < self.game_height-1: # until the block is at the bottom of the board
             self._update_block()
+
             # The animat views the board with its two sensors. Has a view of one unit to the left or right.
-            if self.block_pos[1] == self.paddle_pos:
-                sens_in = [10,10]
-                #print("sensed block on both sensors")
-            elif self.block_pos[1] - 1 == self.paddle_pos:
+            if self.block_pos[1] == self.paddle_pos - 1 or self.block_pos[1] + self.block_size-1 == self.paddle_pos -1:
                 #print("sensed block on right sensor")
-                sens_in = [0,10]
-            elif self.block_pos[1] + 1 == self.paddle_pos:
-                sens_in = [10,0]
+                sens_in = [1,0]
+            elif self.block_pos[1] == self.paddle_pos +1 or self.block_pos[1] + self.block_size-1 == self.paddle_pos +1:
+                sens_in = [0,1]
                 #print("sensed block on left sensor")
             else:
-                sens_in = [-10,-10]
+                sens_in = [0,0]
                 #print("didnt sense block")
 
-            motor_out = animat.activate(sens_in)
-            self._update_paddle(motor_out)
+            output = animat.activate(sens_in)
+            self._update_paddle(output)
             #self._print_game()
 
         if self.block_pos[0] == self.game_height-1:
-            if self.paddle_pos-1 == self.block_pos[1] or self.paddle_pos == self.block_pos[1] or self.paddle_pos+1 == self.block_pos[1]: # paddle size is 3
-                return 1 # point
-            else:
-                return 0
+            # catch
+            if self.block_size == 1:
+                if self.paddle_pos-1 == self.block_pos[1] or self.paddle_pos == self.block_pos[1] or self.paddle_pos+1 == self.block_pos[1]: # paddle size is 3
+                    return 1 #self.game_width-3 # point
+                else:
+                    return 0
+
+            # avoid
+            elif self.block_size == 2:
+                # is the left side of the block on the right side of the paddle
+                if self.block_pos[1] > self.paddle_pos + 1:
+                    return 1 # ( (self.game_width-3)/2 )
+                # is the right side of the block on the left side of the paddle
+                elif self.block_pos[1] + self.block_size-1 < self.paddle_pos - 1:
+                    return 1 # ( (self.game_width-3)/2 )
+                else:
+                    return 0
 
     def run_print(self, animat):
         '''
         It takes an animat player to play the game.
         '''
 
+        init1 = 0.0
+        init2 = 0.0
+
+        animat.set_node_value(-1, init1)
+        animat.set_node_value(-2, init2)
+
+        times = [0.0]
+        outputs = [[init1, init2]]
+
         #RESET GAME
+        print(f"NEW GAME!")
         self.block_pos = [0, random.randint(0,self.game_width-1)] # postion in y,x / rows, cols
         self.board[self.block_pos[0]][self.block_pos[1]] = 1
         self.paddle_pos = int(self.game_width/2) # along the x-axis / cols
 
         while self.block_pos[0] < self.game_height-1: # until the block is at the bottom of the board
+            print("GAME UPDATE")
             self._update_block()
-            # The animat views the board with its two sensors. Has a view of one unit to the left or right.
-            if self.block_pos[1] == self.paddle_pos:
-                sens_in = [10,10]
-                print("sensed block on both sensors")
-            elif self.block_pos[1] - 1 == self.paddle_pos:
-                print("sensed block on right sensor")
-                sens_in = [0,10]
-            elif self.block_pos[1] + 1 == self.paddle_pos:
-                sens_in = [10,0]
-                print("sensed block on left sensor")
-            else:
-                sens_in = [-10,-10]
-                print("didnt sense block")
 
+            for i in range(10):
+                # The animat views the board with its two sensors. Has a view of one unit to the left or right.
+                if self.block_pos[1] == self.paddle_pos:
+                    sens_in = [0.8,0.8]
+                    print("sensed block on both sensors")
+                elif self.block_pos[1] - 1 == self.paddle_pos:
+                    print("sensed block on right sensor")
+                    sens_in = [0.8,0]
+                elif self.block_pos[1] + 1 == self.paddle_pos:
+                    sens_in = [0,0.8]
+                    print("sensed block on left sensor")
+                else:
+                    sens_in = [0.1,0.1]
+                    print("didnt sense block")
+
+                #self._print_game()
+                print(f'PADDLE POS IS {self.paddle_pos}')
+                print(f'BLOCK POS is {self.block_pos[1]}')
+
+                output = animat.advance(sens_in, 0.002, 0.002)
+                times.append(animat.time_seconds)
+                outputs.append(output)
+                self._update_paddle(outputs[-1])
+                print(f'MOTOR OUT: {outputs[-1]}')
+                #animat.reset()
             #self._print_game()
-            print(f'PADDLE POS IS {self.paddle_pos}')
-            print(f'BLOCK POS is {self.block_pos[1]}')
 
-            motor_out = animat.activate(sens_in)
-            print(f'MOTOR OUT: {motor_out}')
-            self._update_paddle(motor_out)
-
-
+        #print(f'MOTOR OUT: {outputs}')
 
         if self.block_pos[0] == self.game_height-1:
             if self.paddle_pos-1 == self.block_pos[1] or self.paddle_pos == self.block_pos[1] or self.paddle_pos+1 == self.block_pos[1]: # paddle size is 3
