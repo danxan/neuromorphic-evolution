4fee5072b308ebd7a01dc9a3fdb51c2222fdd1bc
Daniel Sander Isaksen <daniesis@ml1.uio.no>
2020-04-15 01:29:34 +0200

    replication of albantakis, except logic functions are precoded aggregation functions instead of HMG (as earlier, but added copy and not). game_widt=16, game_height=36, direction is fixed for entire run, Only include Task 1 (from paper)... Improved the implementation of boundaryless. 10 000 instead of 60 000 gens, mutate rates/powers are increased with a power of ten. Implemented exponential fitness measure.

--- source/discrete_experiment/evoalg.py
+++ source/discrete_experiment/evoalg.py
@@ -114,7 +114,7 @@ def run(config_file):
     p = neat.Population(config)
     # Restore from checkpoint
     print("restore pop")
-    p = neat.Checkpointer.restore_checkpoint("neat-checkpoint-9999")
+    p = neat.Checkpointer.restore_checkpoint("neat-checkpoint-9994")
 
     # Add a stdout reporter to show progress in the terminal
     p.add_reporter(neat.StdOutReporter(True))
@@ -123,7 +123,7 @@ def run(config_file):
     p.add_reporter(neat.Checkpointer(5))
 
     # Run for up to 300 generations.
-    num_gen = 2
+    num_gen = 100
     winner = p.run(eval_genomes, num_gen)
 
 
@@ -164,7 +164,7 @@ def run(config_file):
     visualize.plot_species(stats, view=True)
     visualize.draw_net(config, winner, True, node_names=node_names, show_disabled=False, prune_unused=True)
 
-    p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-9999')
+    p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-9994')
     p.run(eval_genomes, 10)
 if __name__ == '__main__':
     # Detemine path to configuration file. This path manipulation is
--- source/discrete_experiment/save_data.sh
+++ source/discrete_experiment/save_data.sh
@@ -1,6 +1,6 @@
 datevar=140400082020
 mkdir $datevar
-cp avg_fitness.svg config-ff ffnn.log Digraph.gv Digraph.gv.svg neat-checkpoint-9999 speciation.svg $datevar
-git add -f save_data.sh avg_fitness.svg config-ff ffnn.log Digraph.gv Digraph.gv.svg neat-checkpoint-9999 speciation.svg evoalg.py game.py
+cp avg_fitness.svg config-ff ffnn.log Digraph.gv Digraph.gv.svg neat-checkpoint-10089 speciation.svg $datevar
+git add -f save_data.sh avg_fitness.svg config-ff ffnn.log Digraph.gv Digraph.gv.svg neat-checkpoint-10089 speciation.svg evoalg.py game.py
 git commit -m "replication of albantakis, except logic functions are precoded aggregation functions instead of HMG (as earlier, but added copy and not). game_widt=16, game_height=36, direction is fixed for entire run, Only include Task 1 (from paper)... Improved the implementation of boundaryless. 10 000 instead of 60 000 gens, mutate rates/powers are increased with a power of ten. Implemented exponential fitness measure."
 
