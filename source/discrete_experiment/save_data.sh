fc8882463fbe60fbc977ac2177a0320c21f9df21
Daniel Sander Isaksen <daniesis@ml1.uio.no>
2020-04-23 22:18:38 +0200

    replication of albantakis, except logic functions are precoded aggregation functions instead of HMG (as earlier, but added copy and not). game_widt=16, game_height=32, direction is fixed for entire run, Only include Task 1 (from paper)... Improved the implementation of boundaryless. 10000 instead of 60 000 gens, mutate rates/powers are increased with a power of ten. Implemented exponential fitness measure. Tried to run with the same logic as quick test. Still same results (60% fitness). NEW: Will evolve topologies with HIGH mutation rate and high pop size

--- source/discrete_experiment/save_data.sh
+++ source/discrete_experiment/save_data.sh
@@ -1,6 +1,6 @@
-datevar=230401302020
+datevar=230422172020
 mkdir $datevar
-cp avg_fitness.svg config-ff ffnn.log Digraph.gv Digraph.gv.svg neat-checkpoint-467 speciation.svg $datevar
-git add -f save_data.sh avg_fitness.svg config-ff ffnn.log Digraph.gv Digraph.gv.svg neat-checkpoint-467 speciation.svg evoalg.py game.py
-git commit -m "replication of albantakis, except logic functions are precoded aggregation functions instead of HMG (as earlier, but added copy and not). game_widt=16, game_height=32, direction is fixed for entire run, Only include Task 1 (from paper)... Improved the implementation of boundaryless. 10000 instead of 60 000 gens, mutate rates/powers are increased with a power of ten. Implemented exponential fitness measure. Tried to run with the same logic as quick test. Still same results (60% fitness). NEW: Will evolve topologies with low mutation rate."
+cp avg_fitness.svg config-ff ffnn.log Digraph.gv Digraph.gv.svg neat-checkpoint-61 speciation.svg $datevar
+git add -f save_data.sh avg_fitness.svg config-ff ffnn.log Digraph.gv Digraph.gv.svg neat-checkpoint-61 speciation.svg evoalg.py game.py
+git commit -m "replication of albantakis, except logic functions are precoded aggregation functions instead of HMG (as earlier, but added copy and not). game_widt=16, game_height=32, direction is fixed for entire run, Only include Task 1 (from paper)... Improved the implementation of boundaryless. 10000 instead of 60 000 gens, mutate rates/powers are increased with a power of ten. Implemented exponential fitness measure. Tried to run with the same logic as quick test. Still same results (60% fitness). NEW: Will evolve topologies with HIGH mutation rate and high pop size"
 
