c09366b6092aa575829ad49dbffac42c6f535a08
Daniel Sander Isaksen <daniesis@ml1.uio.no>
2020-06-03 16:44:59 +0200

    60k gen with popsize 150 and fixed topology. Low compability rate (1). Max stagnation 10. Species Elitism 5. Elitism 5. Best fitness was 90

--- source/discrete_B-fixed-60k/save_data.sh
+++ source/discrete_B-fixed-60k/save_data.sh
@@ -1,5 +1,5 @@
-datevar=085726052020 # date+%R-%D
+datevar=164203062020 # date+%R-%D
 mkdir $datevar
-cp avg_fitness-recurrent.svg config-ff recurrentnn.log neat-checkpoint-23969 $datevar
-git add -f save_data.sh avg_fitness-recurrent.svg config-ff recurrentnn.log neat-checkpoint-23969 evoalg.py game.py
-git commit -m "60k gen with popsize 100 and fixed topology. Low compability rate (1). Max stagnation 1. best fitness was 74, wantto see this network if possible"
+cp avg_fitness-recurrent.svg config-ff recurrentnn.log neat-checkpoint-59952 $datevar
+git add -f save_data.sh avg_fitness-recurrent.svg config-ff recurrentnn.log neat-checkpoint-59952 evoalg.py game.py
+git commit -m "60k gen with popsize 150 and fixed topology. Low compability rate (1). Max stagnation 10. Species Elitism 5. Elitism 5. Best fitness was 90"
