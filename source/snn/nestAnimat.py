ae2074c0d81441fb73bcced0071f8d8a751455b0
Daniel Sander Isaksen <daniesis@uio.no>
2020-08-17 13:27:19 +0200

    trying to add generators and detectors to the animat class

--- source/snn/nestAnimat.py
+++ source/snn/nestAnimat.py
@@ -2,7 +2,7 @@ from nest import *
 from nestGenome import Genome
 
 class Animat(object):
-    def __init__(self, sgs, sds, genome):
+    def __init__(self, genome, sds):
         self.ps = genome.ps # population size / neurons per node
         self.ni = genome.ni # num input nodes
         self.nh = genome.nh # num hidden nodes
@@ -12,8 +12,14 @@ class Animat(object):
         self.hw = genome.hw
         self.ow = genome.ow
 
-        self.sgs = sgs
+        self.sgs = []
+        for i in range(self.ni):
+            self.sgs.append(Create('spike_generator'))
+
         self.sds = sds
+        #self.sds = []
+        #for o in range(self.no):
+        #    self.sds.append(Create('spike_detector'))
 
         self.conn = { "in": [],
                  "out": [],
@@ -28,14 +34,12 @@ class Animat(object):
             self.out.append(Create('iaf_cond_exp', self.ps))
 
         for i in range(self.ni):
-            self.conn['in'].append([])
             for j in range(self.nh):
                 syn_dict = {'weight': self.iw[i][j]}
                 Connect(self.sgs[i], self.hid[j], syn_spec=syn_dict)
                 #print(self.conn['in'][i].append(GetConnections(sgs[i],self.hid[j])))
 
         for i in range(self.nh):
-            self.conn['hid'].append([])
             for j in range(self.nh):
                 syn_dict = {'weight': self.hw[i][j]}
                 Connect(self.hid[i], self.hid[j], syn_spec=syn_dict)
@@ -45,10 +49,9 @@ class Animat(object):
             for j in range(self.no):
                 syn_dict = {'weight': self.ow[i][j]}
                 Connect(self.hid[i], self.out[j], syn_spec=syn_dict)
-                self.conn['out'][i].append(GetConnections(self.hid[i],self.out[j]))
 
         for i in range(self.no):
-            Connect(self.out[i], sds[i])
+            Connect(self.out[i], self.sds[i])
 
     def set_weights(self, genome):
         self.iw = genome.iw
--- source/snn/test.py
+++ source/snn/test.py
@@ -5,42 +5,25 @@ from nestGenome import Genome
 import numpy as np
 
 
-g = Genome(5,4, 0)
-sg = []
-sd = []
+g = Genome(5,4, 0, low=0, high=10000)
+
+sds = []
 for i in range(2):
-    sg.append(Create('spike_generator'))
-    sd.append(Create('spike_detector'))
-    SetStatus(sg[i], {'spike_times': [1.0]})
+    sds.append(Create('spike_detector'))
 
-a = Animat(sg, sd, g)
-conn = GetConnections(a.hid[0], a.out[0])
-Prepare()
-print(conn)
-Run(10)
 
-for s in sd:
-    print(GetStatus(s))
-
-ResetNetwork()
-
-g.ow = np.zeros((g.nh,g.no))
-
-a.set_weights(g)
-
-for i in range(2):
-    SetStatus(sg[i], {'spike_times': [11.0]})
-
-Run(10)
-
-for s in sd:
-    print(GetStatus(s))
-
-Cleanup()
-
-print(1)
+a = Animat(g, sds)
+SetStatus(a.sgs[0], {'spike_times': [1.0]})
+SetStatus(a.sgs[1], {'spike_times': [10.0]})
+Simulate(20)
+print(GetStatus(a.sgs[0]))
+print(GetStatus(a.sgs[1]))
 
 
 
+print(GetStatus(sds[0]))
+print(GetStatus(sds[1]))
+
+ResetKernel()
 
 
