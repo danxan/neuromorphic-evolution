43499519f42650e929da2f5d7b675196442d881b
Daniel Sander Isaksen <daniesis@ml1.uio.no>
2020-08-26 18:14:36 +0200

    also saved getdata and make

--- source/snn/get_data.py
+++ source/snn/get_data.py
@@ -4,18 +4,32 @@ from nestGenome import Genome
 
 import sys
 import pickle
+import numpy as np
 
-filename = str(sys.argv[1])
+def get_data():
+    filename = str(sys.argv[1])
 
-log = None
-with open(filename, 'rb') as f:
-    log = pickle.load(f)
+    log = None
+    with open(filename, 'rb') as f:
+        log = pickle.load(f)
 
-print("ScoreMax: \n{}".format(log['scoreMax']))
-print("ScoreMean: \n{}".format(log['scoreMean']))
-print("genome iw: \n{}\n\
-    genome hw: \n{} \n\
-    genome ow: \n{} \n".format(log['best_solution'].iw, log['best_solution'].hw, log['best_solution'].ow))
+    print("ScoreMax: \n{}".format(log['scoreMax']))
+    print("ScoreMean: \n{}".format(log['scoreMean']))
+    print("genome iw: \n{}\n\
+        genome hw: \n{} \n\
+        genome ow: \n{} \n".format(log['best_solution'].iw, log['best_solution'].hw, log['best_solution'].ow))
+
+
+    return log
+
+
+def mean_scoreMax():
+
+    log = get_data()
+    scoreMax = log['scoreMax']
+
+    return np.mean(scoreMax)
+
 
 
 
